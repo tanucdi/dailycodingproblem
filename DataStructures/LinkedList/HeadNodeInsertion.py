6407b706071f01aca89f1afcf37fdbea42cb974a
tanishk <tanucdi7@gmail.com>
2021-04-20 17:40:05 +0530

    headnodeinsertion

---
+++ DataStructures/LinkedList/HeadNodeInsertion.py
@@ -0,0 +1,56 @@
+#create a class node 
+# node consists of two things - data and next pointer
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+#create a class for LINKED LIST
+#which contains Head pointer
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    #create an function to add node to linked list 
+    def insert(self,newdata):
+        #create a new node by giving new data
+        newnode = Node(newdata)
+
+        #check if head pointer is NONE or It is pointing to someone
+        #if it is none - assign the newnode to head
+        if self.head == None:
+            self.head = newnode
+        else:
+            #if head is already pointing to someone then traverse and find the last node and last.next = none
+            #put the newnode to the last node.
+            last = self.head
+            while (last.next):
+                last = last.next
+            last.next = newnode
+    
+    def HeadNodeInsert(self,newdata):
+        newheadnode = Node(newdata)
+
+        if self.head == None:
+            self.head = newheadnode
+        else:
+            newheadnode.next=self.head
+            self.head=newheadnode
+    #create a function to print our linked list
+    #traverse through linked list and print data
+    def PrintLinkedList(self):
+        current = self.head
+        while current:
+            print(current.data)
+            current=current.next
+
+
+#create an object of linked list
+linkedlist = LinkedList()
+linkedlist.insert(203)
+linkedlist.insert(927)
+linkedlist.HeadNodeInsert(2)
+linkedlist.HeadNodeInsert(13)
+linkedlist.PrintLinkedList()
