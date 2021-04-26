0637b22e973c11627acdc647e09f6d8a1a93012f
tanishk <tanucdi7@gmail.com>
2021-04-26 18:14:30 +0530

    mergetwosortedlist

---
+++ DataStructures/LinkedList/Questions/03_MergeTwoSortedLists.py
@@ -0,0 +1,71 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def Insert(self,val):
+        newnode = Node(val)
+        if self.head ==None:
+            self.head=newnode
+        else:
+            last = self.head
+            while last.next:
+                last=last.next
+            last.next =newnode
+    
+
+    def PrintList(self):
+        current = self.head
+        while current:
+            print(current.data)
+            current=current.next
+
+
+def MergeList(headA,headB):
+# A dummy node to store the result
+    dummyNode = Node(0)
+  
+    # Tail stores the last node
+    tail = dummyNode
+    while True:
+        # If any of the list gets completely empty
+        # directly join all the elements of the other list
+        if headA is None:
+            tail.next = headB
+            break
+        if headB is None:
+            tail.next = headA
+            break
+        # Compare the data of the lists and whichever is smaller is
+        # appended to the last of the merged list and the head is changed
+        if headA.data <= headB.data:
+            print(headA.data)
+            tail.next = headA
+            headA = headA.next
+        else:
+            print(headB.data)
+            tail.next = headB
+            headB = headB.next
+  
+        # Advance the tail
+        tail = tail.next
+  
+    # Returns the head of the merged list
+    return dummyNode.next
+
+l1=LinkedList()
+l2=LinkedList()
+l1.Insert(1)
+l1.Insert(2)
+l1.Insert(4)
+l2.Insert(1)
+l2.Insert(3)
+l2.Insert(5)
+l2.Insert(6)
+#to call the function
+l2.head=MergeList(l1.head,l2.head)
+l2.PrintList()
