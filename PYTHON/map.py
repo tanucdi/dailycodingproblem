ec8e6c8f1fc25bc155abfa6e555c4b90e632f0a3
tanishk <tanucdi7@gmail.com>
2021-06-07 07:42:56 +0530

    map added

---
+++ PYTHON/map.py
@@ -0,0 +1,20 @@
+'''
+map() function returns a map object(which is an iterator) of the results after applying the given function to each item of a given iterable (list, tuple etc.)
+
+Syntax :
+
+map(fun, iter)
+Parameters :
+
+fun : It is a function to which map passes each element of given iterable.
+iter : It is a iterable which is to be mapped.
+
+NOTE : You can pass one or more iterable to the map() function.
+'''
+# Add two lists using map and lambda
+
+numbers1 = [1, 2, 3]
+numbers2 = [4, 5, 6]
+
+result = map(lambda x, y: x + y, numbers1, numbers2)
+print(list(result))
